2c2b3befea8d248f6263483d4ded685ab33e2426
Costas Kirgoussios <ckir1508@hotmail.com>
2022-08-19 10:52:02 +0300

    Add sleep in case it fails immediately

--- StartHTTPWatchdog_Windows.ps1
+++ StartHTTPWatchdog_Windows.ps1
@@ -2,7 +2,7 @@ $counter = 0
 while ($true) {
     $counter++
     Write-Host "Running $counter times"
-    node.exe HTTPWatchdog.js --port=9999 --timeout=300000node HTTPWatchdog.js --port=9999 --timeout=300000
+    node.exe HTTPWatchdog.js --port=9999 --timeout=300000
     Start-Sleep -Seconds 1.5
 }
 
